95cdde734fa9ca02414371606379214c8f168def
Дарья Соколова <anda0311@yandex.ru>
2023-02-02 23:56:21 +0400

    prefect

--- test2.py
+++ test2.py
@@ -1,21 +1,21 @@
+from prefect import flow, task
 import pandas as pd
-from dagster import asset, get_dagster_logger
-from sklearn.model_selection import train_test_split
-from catboost import Pool, CatBoostRegressor
-from sklearn.metrics import r2_score, mean_absolute_error, mean_squared_error
 
-@asset
+@task
 def df_name():
-    # assign data of lists.
+     # assign data of lists.
     data = {'name': ['wikipedia', 'google', 'mail'], 'url': ['https://ru.wikipedia.org/wiki/%D0%AF%D0%BD%D0%B4%D0%B5%D0%BA%D1%81u', 'https://www.google.com/intl/ru_ru/drive/', 'https://pogoda.mail.ru/prognoz/saratov/']}
     # Create DataFrame
     df = pd.DataFrame(data)
-    df.to_csv('df_name.csv')
+    df.to_csv('df_name-1.csv')
     return df
-
-@asset
-def df_result(df_name):
-    df_result = df_name
+    
+@flow(log_prints=True, name="prefect-example-hello-flow")
+def df_result():
+    df_result = df_name()
     df_result['domain_of_url'] = df_result['url'].map(lambda x: str(x).partition("://")[2].partition("/")[0])
-    df_result.to_csv('df_result.csv')
+    df_result.to_csv('df_result-1.csv')
+    
     return df_result
+if __name__ == "__main__":
+    df_result()

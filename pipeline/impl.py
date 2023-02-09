f24b3540cb575d35e44fdde81abcdedc2ee6c77c
Дарья Соколова <anda0311@yandex.ru>
2023-02-09 17:55:59 +0400

    я честно пыталась, но я не понимаю...

---
+++ pipeline/impl.py
@@ -0,0 +1,10 @@
+class Pipeline:
+    def run():
+        pass
+
+class BaseTask:
+    def run():
+        pass
+
+class CopyToFile(BaseTask):
+    pass
--- pipeline/pipeline.py
+++ pipeline/pipeline.py
@@ -1,19 +1,17 @@
 from pipelines import tasks, Pipeline
-from pipelines.tasks import sql_create_table_as, load_file_to_db, save_table_to_file
+from pipelines.tasks import (
+    sql_create_table_as, 
+    load_file_to_db, 
+    save_table_to_file
+)
 
 NAME = 'test_project'
 SCHEMA = 'public'
 YEAR_SUFFIX = '2023'
+LOAD_TASK = load_file_to_db(input='original/original.csv', output='original')
 
-pipeline = Pipeline(
-    name=NAME,
-    schema=SCHEMA,
-    version=VERSION,
-    tasks=[
-        load_file_to_db(
-            input='original/original.csv',
-            output='original',
-        ),
+TASKS = [
+        LOAD_TASK,
         sql_create_table_as(
             table='norm',
             query='''
@@ -30,4 +28,16 @@ pipeline = Pipeline(
         sql('drop table {original}'),
         sql('drop table {norm}'),
     ]
+
+
+pipeline = Pipeline(
+    name=NAME,
+    schema=SCHEMA,
+    version=VERSION,
+    tasks=TASKS
 )
+
+if __name__== "__main__":
+    LOAD_TASK.run() -- run particular task
+    pipline.run() -- run pipelinres of tasks
+    
